e8497222dd4f2966bc3694d72e553e8bbd38e497
frat-i <pratafourever@gmail.com>
2022-10-12 21:25:07 -0300

    Adicionando ExercicioResolvidoEnum

---
+++ src/Secao8/ExercicioResolvidoEnum/ExercicioResolvidoEnum/Entities/Departament.cs
@@ -0,0 +1,17 @@
+﻿namespace ExercicioResolvidoEnum.Entities
+{
+    internal class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Secao8/ExercicioResolvidoEnum/ExercicioResolvidoEnum/Entities/HourContract.cs
@@ -0,0 +1,26 @@
+﻿namespace ExercicioResolvidoEnum.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valueperhour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valueperhour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
