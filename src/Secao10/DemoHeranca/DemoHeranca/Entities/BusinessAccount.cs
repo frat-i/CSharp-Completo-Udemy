7e26ebbcd89f0de8d828a246b9ef7715be8e0295
frat-i <pratafourever@gmail.com>
2022-10-14 18:28:03 -0300

    Renomeando Secao8 e adicionando Secao10

---
+++ src/Secao10/DemoHeranca/DemoHeranca/Entities/BusinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoHeranca.Entities
+{
+    internal class BusinessAccount : Account // <- para colocar herança é só usar o : + nome da classe 
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ src/Secao10/DemoHeranca/DemoHeranca/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DemoHeranca.Entities;
+namespace DemoHeranca;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 6900.00, 1000.00) ;
+
+        Console.WriteLine(account.Balance);
+
+    }
+}
