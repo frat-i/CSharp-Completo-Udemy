c0ce488dac3cc4204ad86f894c90ba14094c60b5
frat-i <pratafourever@gmail.com>
2022-10-17 20:35:54 -0300

    Update ExercicioResolvidoMA

---
+++ src/Secao10/ExercicioRMetodosAbstratos/ExercicioRMetodosAbstratos/Program.cs
@@ -0,0 +1,54 @@
+﻿using ExercicioRMetodosAbstratos.Entities;
+using System;
+using ExercicioRMetodosAbstratos.Entities.Enum;
+using System.Globalization;
+
+namespace ExRMetodosAbstratos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<Shape> list = new List<Shape>();
+
+        Console.Write("Enter the number of shapes: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine($"Shape #{i} data: ");
+            Console.Write("Rectangle or Circle (r/c)? ");
+            char ch = char.Parse(Console.ReadLine().ToString().ToLower());
+            Console.Write("Color (Black/Blue/Red): ");
+            Color color = Enum.Parse<Color>(Console.ReadLine());
+
+            if (ch == 'r')
+            {
+                Console.Write("Width: ");
+                double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Height: ");
+                double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Rectangle(width, height, color));
+
+            }
+            else
+            {
+                Console.Write("Radius: ");
+                double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Circle(radius, color));
+            }
+
+
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Shape Areas: ");
+        foreach (Shape shape in list)
+        {
+            Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+    }
+}
