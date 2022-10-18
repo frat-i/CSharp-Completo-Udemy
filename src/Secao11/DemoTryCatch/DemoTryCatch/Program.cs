7d316b62a1fa6e1c436871e2f1613e966d9f97c1
frat-i <pratafourever@gmail.com>
2022-10-18 17:46:22 -0300

    Adicionando DemoTryCatch

---
+++ src/Secao11/DemoTryCatch/DemoTryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoTryCatch;
+
+class Program{
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int result = n1 / n2;
+
+            Console.WriteLine(result);
+        }
+
+        catch(DivideByZeroException) // nesse caso nem precisaria dar apelido pra excecao
+        {
+            Console.WriteLine("Division by zero is not allowed");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine($"Format error {e.Message}");
+        }
+    }
+}
