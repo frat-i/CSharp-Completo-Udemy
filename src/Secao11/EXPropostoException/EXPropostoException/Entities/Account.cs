eec5c3cbaa500c6bd9d78fbc905f14bc06f97235
frat-i <pratafourever@gmail.com>
2022-10-18 19:48:05 -0300

    Adicionando ExercicioPropostoResolvido

---
+++ src/Secao11/EXPropostoException/EXPropostoException/Entities/Account.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXPropostoException.Entities.Exceptions;
+
+namespace EXPropostoException.Entities
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int accountNumber, string holder, double balance, double withdrawLimit)
+        {
+            AccountNumber = accountNumber;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if(Balance == 0)
+            {
+                throw new DomainException("The account balance needs to be more than Zero to make a withdraw");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds the withdraw limit");
+            }
+            if(amount > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= amount;
+        }
+
+
+    }
+}
---
+++ src/Secao11/EXPropostoException/EXPropostoException/Program.cs
@@ -0,0 +1,40 @@
+﻿using EXPropostoException.Entities;
+using EXPropostoException.Entities.Exceptions;
+using System;
+
+namespace EXPException;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter account data: ");
+            Console.Write("Number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string accountHolder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double accountBalance = double.Parse(Console.ReadLine());
+            Console.Write("Withdraw limit: ");
+            double accountWithdrawLimit = double.Parse(Console.ReadLine());
+
+            Account account = new(accountNumber, accountHolder, accountBalance, accountWithdrawLimit);
+            Console.WriteLine();
+
+            Console.Write("Enter amount for withdraw: ");
+            double amountWithdraw = double.Parse(Console.ReadLine());
+            account.Withdraw(amountWithdraw);
+            Console.Write($"New balance: {account.Balance}");
+        }
+        catch(DomainException e)
+        {
+            Console.WriteLine($"Withdraw error: {e.Message}");
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine($"Format error : {e.Message}");
+        }
+    }
+}
