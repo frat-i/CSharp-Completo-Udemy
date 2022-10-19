46d6726351d4df06ef660bd9dd4bf3eb0552d4c5
frat-i <pratafourever@gmail.com>
2022-10-19 19:55:22 -0300

    Tratando erros com exceções personalizadas

--- src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Program.cs
+++ src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Program.cs
@@ -3,6 +3,7 @@ using XadrezTabuleiro.Entities;
 using XadrezTabuleiro.Entities.Enums;
 using Xadrez_Console.Presentation.Entities;
 using Xadrez_Console.Chess.Entities;
+using Xadrez_Console.Board.Entities.Exceptions;
 
 namespace Xadrez_Console;
 
@@ -10,16 +11,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        try
+        {
+            Tabuleiro tab = new(8, 8);
 
-        Tabuleiro tab = new(8, 8);
+            tab.ColocarPeca(new Torre(tab, Cor.Laranja), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(7, 2));
 
-        tab.ColocarPeca(new Torre(tab, Cor.Laranja), new Posicao(0,0));
-        tab.ColocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(1,3));
-        tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
-        
-        Tela.ImprimirTabuleiro(tab);
+            Tela.ImprimirTabuleiro(tab);
 
-
+        }
+        catch(TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
 
     }
 }
