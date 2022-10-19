805756dd701453ae35fece3794fa55ddbad5af8c
frat-i <pratafourever@gmail.com>
2022-10-19 20:52:21 -0300

    Melhorando Implementação Método ImprimirTabuleiro

--- src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Presentation/Entities/Tela.cs
+++ src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Presentation/Entities/Tela.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using XadrezTabuleiro.Entities;
+using XadrezTabuleiro.Entities.Enums;
 
 namespace Xadrez_Console.Presentation.Entities
 {
@@ -13,21 +14,42 @@ namespace Xadrez_Console.Presentation.Entities
         {
             for(int i = 0; i < Tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < Tab.Colunas; j++)
                 {
+                    
                     if(Tab.Peca(i,j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(Tab.Peca(i, j) + " ");
+                        Tela.ImprimirPeca(Tab.Peca(i, j));
+                        Console.Write(" ");
                     }
                    
                 }
                 Console.WriteLine();
             }
-            
+            Console.WriteLine("  a b c d e f g h");   
         }
+
+
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+        }
+
     }
 }
--- src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Program.cs
+++ src/Secao12_ProjetoXadrez/Xadrez-Console/Xadrez-Console/Program.cs
@@ -13,9 +13,23 @@ class Program
     static void Main(string[] args)
     {
 
-        PosicaoXadrez pos = new('c', 7);
-        Console.WriteLine(pos);
-        Console.WriteLine(pos.ToPosicao());
+        try
+        {
+            Tabuleiro tab = new(8, 8);
 
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(5, 2));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(4, 7));
+
+            tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 1));
+            tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 1));
+            tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 5));
+
+            Tela.ImprimirTabuleiro(tab);
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
