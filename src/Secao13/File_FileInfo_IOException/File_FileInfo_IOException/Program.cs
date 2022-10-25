fb73bc9ca1ee356f1cbbf9b6ee091f70c8d6a0aa
frat-i <pratafourever@gmail.com>
2022-10-24 23:02:18 -0300

    Adicionando PrimeiraAula Secao13

---
+++ src/Secao13/File_FileInfo_IOException/File_FileInfo_IOException/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AulaArquivos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string sourcepath = @"d:\file1.txt";
+        string targetpath = @"d:\file2.txt"; // clonar arquivo
+
+        try
+        {
+            FileInfo fileinfo = new FileInfo(sourcepath); // criou um objeto que está ligado ao arquivo sourcepath
+            fileinfo.CopyTo(targetpath); //copia arquivo para o lugar q ta nos ()
+
+            string[] lines = File.ReadAllLines(sourcepath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
