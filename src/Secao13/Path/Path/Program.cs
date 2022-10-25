f98b7ede9c41cddfbe4acda901f5a2c434e882a8
frat-i <pratafourever@gmail.com>
2022-10-25 19:50:58 -0300

    Adicionando Path

---
+++ src/Secao13/Path/Path/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EXPath;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"d:\teste\file1.txt";
+
+
+        Console.WriteLine($"DirectorySeparatorChar: {Path.DirectorySeparatorChar}"); //mostra a \ pq é ela que ta separando os diretórios
+        Console.WriteLine($"PathSeparator: {Path.PathSeparator}");
+        Console.WriteLine($"GetDirectoryName: {Path.GetDirectoryName(path)}");
+        Console.WriteLine($"GetFileName: {Path.GetFileName(path)}");
+        Console.WriteLine($"GetFileNameWithoutExtension: {Path.GetFileNameWithoutExtension(path)}");
+        Console.WriteLine($"GetExtension: {Path.GetExtension(path)}");
+        Console.WriteLine($"GetFullPath: {Path.GetFullPath(path)}");
+        Console.WriteLine($"GetTempPath: {Path.GetTempPath()}"); //mostra caminho para a pasta temp do windows
+
+
+
+    }
+}
