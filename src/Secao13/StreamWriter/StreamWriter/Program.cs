bc5d6d6303b2fcac0a7b0f5fafc0ff3ea71ad8f9
frat-i <pratafourever@gmail.com>
2022-10-25 12:08:53 -0300

    Adicionando Exemplo_StreamWriter

---
+++ src/Secao13/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace StreamWriter_EX;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string sourcepath = @"d:\file1.txt";
+        string targepath = @"d:\file2.txt";
+
+        try
+        {
+            string[] lines = File.ReadAllLines(sourcepath);
+
+            using (StreamWriter sw = File.AppendText(targepath))
+            {
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error has occurred");
+            Console.WriteLine(e.Message);
+        }
+
+
+
+    }
+}
