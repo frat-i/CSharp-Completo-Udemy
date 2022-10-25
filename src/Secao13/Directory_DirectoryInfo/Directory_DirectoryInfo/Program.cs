687ebe46afb8d02de8a09f14cef7808c9d2a947c
frat-i <pratafourever@gmail.com>
2022-10-25 19:31:56 -0300

    Adicionando Directory_DirectoryInfo

---
+++ src/Secao13/Directory_DirectoryInfo/Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EXDirectory_DirectoryInfo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"d:\teste\";
+
+        try
+        {
+            //listar subpastas -> *.* todos nomes e extensoes
+            IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); // poderia usar var que já seria colocado como IEnumerable
+            Console.WriteLine("FOLDERS: ");
+
+            foreach (string folder in folders)
+            {
+                Console.WriteLine(folder);
+            }
+
+            Console.WriteLine();
+
+            //listar arquivos
+            IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            Console.WriteLine("FILES: ");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Directory.CreateDirectory(path + @"\newfolder");
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error has occurred");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
