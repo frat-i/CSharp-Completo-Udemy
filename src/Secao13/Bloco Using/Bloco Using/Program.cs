c5ce1233bc37c3b3926480de357b3325e93e19ee
frat-i <pratafourever@gmail.com>
2022-10-25 12:03:18 -0300

    Adicionando Bloco_Using

---
+++ src/Secao13/Bloco Using/Bloco Using/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Bloco_Using;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"d:\file1.txt";
+
+
+        try
+        {
+            /*using (FileStream fs = new(path, FileMode.Open))*/ //using automaticamente fecha coisas não verificadas pelo CLR -> filestream,streamreader...
+                                                                 //{
+            using (StreamReader sr = File.OpenText(path)) // -> forma resumida
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            //}
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error has occurred");
+            Console.WriteLine(e.Message);
+        }
+
+
+    }
+}
