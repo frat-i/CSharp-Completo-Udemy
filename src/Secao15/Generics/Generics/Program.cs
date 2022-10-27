8fcd217f2e2cc3e3b777ed896f9a034113205d9e
frat-i <pratafourever@gmail.com>
2022-10-27 19:21:50 -0300

    Adicionando ExemploGenerics

---
+++ src/Secao15/Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExemploGenerics;
+class Program
+{
+	static void Main(string[] args)
+	{
+		PrintService<int> printservice = new PrintService<int>();
+
+		Console.Write("How many values? ");
+		int n = int.Parse(Console.ReadLine());
+		for (int i = 0; i < n; i++)
+		{
+			int x = int.Parse(Console.ReadLine());
+			printservice.AddValue(x);
+		}
+
+		//int a = printservice.First();
+		//int b = a + 2;
+		//Console.WriteLine(b);
+
+
+		printservice.Print();
+		Console.Write($"First: {printservice.First()}");
+	}
+}
