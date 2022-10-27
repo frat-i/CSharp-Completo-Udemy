c143834d147ef3c3971e239cc649e721906313be
frat-i <pratafourever@gmail.com>
2022-10-27 18:17:32 -0300

    Adicionando Exemplo IComparable

---
+++ src/Secao14/IComparable/IComparable/Program.cs
@@ -0,0 +1,35 @@
+﻿using EXIEComparable.Entities;
+
+namespace EXIEComparable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"d:\teste\in.txt";
+        try
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                List<Employee> list = new List<Employee>();
+                while (!sr.EndOfStream) // enquanto não é o final do texto
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+
+                list.Sort();
+                foreach (Employee emp in list)
+                {
+                    Console.WriteLine(emp);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error has occurred");
+            Console.WriteLine(e.Message);
+        }
+
+
+    }
+}
