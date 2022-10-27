1b2d58ddbe125283333488b4ad0d7265d9589abe
frat-i <pratafourever@gmail.com>
2022-10-27 20:17:51 -0300

    Adicionando GetHashCode e Equals

---
+++ src/Secao15/GetHashCodeEEquals/GetHashCodeEEquals/Entities/Client.cs
@@ -0,0 +1,32 @@
+﻿namespace GetHashCodeEEquals.Entities;
+internal class Client
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public Client()
+    {
+
+    }
+
+    public Client(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (!(obj is Client))
+        {
+            return false;
+        }
+        Client client = (Client)obj;
+        return Email.Equals(client.Email);
+    }
+
+    public override int GetHashCode()
+    {
+        return Email.GetHashCode();
+    }
+}
---
+++ src/Secao15/GetHashCodeEEquals/GetHashCodeEEquals/Program.cs
@@ -0,0 +1,35 @@
+﻿using GetHashCodeEEquals.Entities;
+
+namespace ExemploGHSEquals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Client a = new("Maria", "maria@gmail.com");
+        Client b = new("Alex", "maria@gmail.com");
+
+        Console.WriteLine(a.Equals(b)); //vai dar true pq atendeu aos criterios
+        Console.WriteLine(a == b); // vai dar false pq sao dois objs diferentes
+        Console.WriteLine(a.GetHashCode());
+        Console.WriteLine(b.GetHashCode());
+
+
+
+
+
+
+        //string a = "Maria";
+        //string b = "Maria";
+
+        ////Console.WriteLine(a.Equals(b));
+
+        //Console.WriteLine(a.GetHashCode());
+        //Console.WriteLine(b.GetHashCode());
+
+
+
+
+    }
+}
