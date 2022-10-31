a69971c9ab966a4ff8f5112da2c749334c7d6ad3
frat-i <pratafourever@gmail.com>
2022-10-31 18:02:00 -0300

    Adicionando Exemplo Comparison

---
+++ src/Secao17/Comparison/Comparison/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExComparison.Entities;
+
+namespace ExComparison;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+
+        list.Add(new Product("TV", 900.00));
+        list.Add(new Product("Notebook", 1200.00));
+        list.Add(new Product("Tablet", 450.00));
+
+        list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); // expressao lambda 
+
+        // list.sort(CompareProducts);
+        // Comparison<Product> comp = CompareProducts;
+        // list.sort(comp);
+
+
+        foreach (Product p in list)
+        {
+            Console.WriteLine(p);
+        }
+    }
+
+    static int CompareProducts(Product p1, Product p2)
+    {
+        return p1.Name.ToUpper().CompareTo((p2.Name.ToUpper()));
+    }
+}
