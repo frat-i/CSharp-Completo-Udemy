a9f48ce0c4a4edafe3f054439775fd1c1a1c1bf9
frat-i <pratafourever@gmail.com>
2022-10-31 18:27:02 -0300

    Adicionando MulticastDelegate

--- src/Secao17/IntroducaoDelegates/IntroducaoDelegates/Program.cs
+++ src/Secao17/IntroducaoDelegates/IntroducaoDelegates/Program.cs
@@ -4,20 +4,26 @@ namespace IntroDelegates;
 
 class Program
 {
-    delegate double BinaryNumericOperation(double n1, double n2);
+    //delegate double BinaryNumericOperation(double n1, double n2);
+    delegate void BinaryNumericOperation(double n1, double n2);
     static void Main(string[] args)
     {
         double a = 10;
         double b = 12;
 
-        BinaryNumericOperation op = CalculationService.Sum;
+
+        //BinaryNumericOperation op = CalculationService.Sum;
 
         //double result = CalculationService.Sum(a, b);
 
-        double result = op(a, b);
+        //double result = op(a, b);
 
-        double result2 = op.Invoke(a, b);
+        //double result2 = op.Invoke(a, b);
 
-        Console.WriteLine(result);
+        BinaryNumericOperation op = CalculationService.ShowSum;
+        op += CalculationService.ShowMax;
+
+        op(a, b);
+
     }
 }
--- src/Secao17/IntroducaoDelegates/IntroducaoDelegates/Services/CalculationService.cs
+++ src/Secao17/IntroducaoDelegates/IntroducaoDelegates/Services/CalculationService.cs
@@ -5,12 +5,26 @@ internal class CalculationService
     {
         return (x > y) ? x : y;
     }
+
     public static double Sum(double x, double y)
     {
         return x + y;
     }
+
     public static double Square(double x)
     {
         return x * x;
+    }
+
+    public static void ShowMax(double x, double y)
+    {
+        double max = (x > y) ? x : y;
+        Console.WriteLine(max);
+    }
+
+    public static void ShowSum(double x, double y)
+    {
+        double sum = x + y;
+        Console.WriteLine(sum);
     }
 }
